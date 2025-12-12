e504b96c565740ead96cc4b11e554f7fbc053d55
ViKi10000 <vigneshvk192002@gmail.com>
2025-12-12 17:48:59 +0530

    feat: update frontend dependencies and implement routing
    - Added new dependencies: @tailwindcss/vite, @tanstack/react-query, axios, react-hot-toast, react-router, and daisyui.
    - Refactored App component to include routing for HomePage, AboutPage, and ProblemsPage.
    - Integrated react-hot-toast for notifications in HomePage.
    - Created new pages: AboutPage, HomePage, and ProblemsPage.
    - Set up axios instance for API calls with base URL from environment variables.
    - Updated index.css to use Tailwind CSS and DaisyUI for styling.
    - Configured Vite to use Tailwind CSS plugin.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,28 @@ import {
   SignInButton,
   SignOutButton,
   UserButton,
+  useUser,
 } from "@clerk/clerk-react";
 import React from "react";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProblemsPage from "./pages/ProblemsPage";
+import { Navigate, Route, Routes } from "react-router";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
+  const { isSignedIn } = useUser();
   return (
     <>
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
-
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
-
-      <UserButton />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route
+          path="/problems"
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
   );
 };
